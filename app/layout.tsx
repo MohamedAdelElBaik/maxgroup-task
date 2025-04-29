0b302213ee34367d9eea3c3980c90d05d03fc34e
Mohamed Adel <ma7598101@gmail.com>
2025-04-29 15:36:45 +0300

    STYLE_FIX: update the OG image location

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
     siteName: 'Adel Drive Clone',
     images: [
       {
-        url: '/og.jpg',
+        url: 'https://maxgroup-task.vercel.app/og.jpg',
         width: 1200,
         height: 630,
         alt: 'Adel Clone Preview',
